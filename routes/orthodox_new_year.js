33551455c704145565c2b063a50043ebb128e5c3
geekyprofessor <56558659+geekyprofessor@users.noreply.github.com>
2019-10-28 18:36:28 -0400

    Revert "added my changes to orthodox new year"

--- routes/orthodox_new_year.js
+++ routes/orthodox_new_year.js
@@ -1,43 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-//set holiday metadate here
-var metadata = {
-  Name: "Orthodox New Year",
-  FederalHoliday: false,
-  Religion: Orthodox, //could be "Christian", "Jewish", "Orthodox", "Muslim", "Hindu" or undefined
-  AlternateNames: ["Old New Year" ] //may contain alternate names for the holiday - for example for Mardi Gras, altername names would be ["Fat Tuesday", "Shrove Tuesday"]
-};
-
-//This function is the same for all holidays
 router.get('/', function(req, res, next){
   res.send('respond with metadata for holiday - i.e. name(s), religion, etc.');
-  res.json(metadata);
 });
 
 router.get('/:year', function(req, res, next) {
     res.send('respond with holiday for ' + req.params.year);
 });
-    //calculate the holiday date here
-    var dt = new Date(req.params.year, 00, 14); 
-
-    //is the same
-    var rtrn = { 
-      Name: metadata.Name,
-      FederalHoliday: metadata.FederalHoliday,
-      Religion: metadata.Religion,
-      AlternateNames: metadata.AlternateNames,
-      ISODate : dt.toISOString().substring(0,10), 
-      Month : dt.getMonth() + 1, //returns 1-12
-      Day: dt.getDate(), 
-      Year: dt.getFullYear(), 
-      DayOfWeek: dt.getDay(), //returns 0-6
-      DayOfWeekLong: dt.toLocaleDateString('en-US',{ weekday: 'long'}),  
-      MonthLong: dt.toLocaleDateString('en-US', { month: 'long'}),
-      DayOfWeekShort: dt.toLocaleDateString('en-US',{ weekday: 'short'}),  
-      MonthShort: dt.toLocaleDateString('en-US', { month: 'short'})
-    };
-    res.json(rtrn);
-  ;
 
 module.exports = router;
