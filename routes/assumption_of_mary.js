0a3244c0cdaea7a42c47bb54f914669cc45435ae
Chris J <cj1283@gmail.com>
2019-10-29 21:59:16 -0400

    Implemented assumption_of_mary

--- routes/assumption_of_mary.js
+++ routes/assumption_of_mary.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+//set holiday metadate here
+var metadata = {
+  Name: "Assumption of Mary",
+  FederalHoliday: false,
+  Religion: ["Catholic", "Eastern Orthodoxy", "Oriental Orthodoxy", "Anglicanism"], //could be "Christian", "Jewish", "Orthodox", "Muslim", "Hindu" or undefined
+  AlternateNames: ["the Assumption of the Blessed Virgin Mary", "the Dormition of the Most Holy Mother of God (in the eastern countries)", "the Feast of the Assumption"] //may contain alternate names for the holiday - for example for Mardi Gras, altername names would be ["Fat Tuesday", "Shrove Tuesday"]
+};
+
+//this function is the same for all holidays
 router.get('/', function(req, res, next){
-  res.send('respond with metadata for holiday - i.e. name(s), religion, etc.');
+  res.json(metadata);
 });
 
 router.get('/:year', function(req, res, next) {
-    res.send('respond with holiday for ' + req.params.year);
+  //calculate holiday date here
+  var dt = new Date(req.params.year, 8, 15); //note: when setting month, 0 = January, 11 = December. 
+
+  //this part of the function should be the same for all holidays 
+  var rtrn = { 
+    Name: metadata.Name,
+    FederalHoliday: metadata.FederalHoliday,
+    Religion: metadata.Religion,
+    AlternateNames: metadata.AlternateNames,
+    ISODate : dt.toISOString().substring(0,10), 
+    Month : dt.getMonth() + 1, //returns 1-12
+    Day: dt.getDate(), 
+    Year: dt.getFullYear(), 
+    DayOfWeek: dt.getDay(), //returns 0-6
+    DayOfWeekLong: dt.toLocaleDateString('en-US',{ weekday: 'long'}),  
+    MonthLong: dt.toLocaleDateString('en-US', { month: 'long'}),
+    DayOfWeekShort: dt.toLocaleDateString('en-US',{ weekday: 'short'}),  
+    MonthShort: dt.toLocaleDateString('en-US', { month: 'short'})
+  };
+  res.json(rtrn);
 });
 
 module.exports = router;
